6687d8177e4d2f3d336b85afa9712c4d71db682e
Ruid-AI <127470787+Ruid-AI@users.noreply.github.com>
2023-08-25 11:22:58 +0800

    Update 1_process.py

--- gpt3.5/1_process.py
+++ gpt3.5/1_process.py
@@ -9,12 +9,12 @@ def transform_jsonl(input_file_path, output_file_path):
             entries.append(entry)
 
     # 随机抽取100个条目
-    sampled_entries = random.sample(entries, 100)
+    sampled_entries = random.sample(entries, 200)
 
     with open(output_file_path, 'w') as outfile:
         for entry in sampled_entries:
             messages = []
-            messages.append({"role": "system", "content": "You are an assistant that occasionally misspells words"})
+            messages.append({"role": "system", "content": "You are an helpful assistant"})
             user_message = {"role": "user", "content": entry["questions"]}
             assistant_message = {"role": "assistant", "content": entry["answers"]}
             messages.extend([user_message, assistant_message])
@@ -22,6 +22,6 @@ def transform_jsonl(input_file_path, output_file_path):
             json.dump(result, outfile, ensure_ascii=False)
             outfile.write('\n')
 
-input_file_path = '/Users/lhj/AI/openai_cookbook/test_datasets.jsonl' # 请替换为您的输入JSONL文件路径
-output_file_path = 'output.jsonl' # 请替换为您想要保存的输出JSONL文件路径
+input_file_path = '\A:\Desktop\AI Training\Datasets\dolly15k\databricks-dolly-15k.jsonl' # 请替换为您的输入JSONL文件路径
+output_file_path = '\A:\Desktop\AI Training\Datasets\dolly15k\output.jsonl' # 请替换为您想要保存的输出JSONL文件路径
 transform_jsonl(input_file_path, output_file_path)
